e01f5f83bdadf48db31569b11ca6c64846c0fbf7
archipunkrockboy <nowrim@mail.ru>
2023-03-03 01:27:49 +0300

    Воспользовался аттрибутами для краткого написания геттеров и сеттеров

--- Lab2/student.rb
+++ Lab2/student.rb
@@ -1,4 +1,8 @@
 class Student
+  attr_reader :surname, :name, :lastname,
+              :id, :phone, :tg, :mail, :git
+  attr_writer :surname, :name, :lastname,
+              :id, :phone, :tg, :mail, :git
   def initialize(surname, name, lastname, optional_parameters={})
     self.surname = surname
     self.name = name
@@ -9,71 +13,7 @@ class Student
     self.mail = optional_parameters[:mail]
     self.git = optional_parameters[:git]
   end
-
-  def id
-    @id
-  end
-
-  def id=(id)
-    @id = id
-  end
-
-  def surname
-    @surname
-  end
-
-  def surname=(surname)
-    @surname = surname
-  end
-  def name
-    @name
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def lastname
-    @lastname
-  end
-
-  def lastname=(lastname)
-    @lastname = lastname
-  end
-
-  def phone
-    @phone
-  end
-
-  def phone=(phone)
-    @phone = phone
-  end
-
-  def tg
-    @tg
-  end
-
-  def tg=(tg)
-    @tg = tg
-  end
-
-  def mail
-    @mail
-  end
-
-  def mail=(mail)
-    @mail = mail
-  end
-
-  def git
-    @git
-  end
-
-  def git=(git)
-    @git = git
-  end
-
-
+  
 end
 
 a = Student.new(1, 2, 3,  {id: 5, mail: 7})
