befb73e07ed2623eaba43bee84dc976e818ca235
archipunkrockboy <nowrim@mail.ru>
2023-03-17 10:34:02 +0300

    реализован метод get_info

--- Lab2/main.rb
+++ Lab2/main.rb
@@ -2,11 +2,17 @@ require "./student"
 require 'json'
 #student1 = Student.new("Месси", "Лионель", "Барселонович", {mail: "hozyainreala@mail.ru"})
 #student2 = Student.new("Криштиану", "Роналду", "Альнасрович", {phone: "+79188374645"})
-student3 = Student.new(surname: "surname", name: "name", lastname: "lastname", git: "@git", tg:"@tg")
+# student3 = Student.new(surname: "surname", name: "name", lastname: "lastname", git: "@git", tg:"@tg")
 
 # student3 = Student.make_from_str("ass name")
 
 
-a ='{"surname": "surname", "name": "name", "lastname": "lastname", "tg": "@eeee"}'
-b = 'aeder'
-s1 = Student.make_student_from_str(b)
+s1 = Student.new(surname: "Manukyan", name: "Artur", lastname: "Vaginakovich", git:"@git", phone:"+79189315711", tg:"@tg")
+s2 = Student.new(surname: "Manukyan", name: "Artur", lastname: "Vaginakovich", git:"@git", phone:"+79189315711", tg:"@tg")
+s3 = Student.new(surname: "Manukyan", name: "Artur", lastname: "Vaginakovich")
+s4 = Student.new(surname: "Manukyan", name: "Artur", lastname: "Vaginakovich", git:"@git", phone:"+79189315711", tg:"@tg")
+
+p s1.get_info
+p s2.get_info
+p s3.get_info
+p s4.get_info
--- Lab2/student.rb
+++ Lab2/student.rb
@@ -27,6 +27,7 @@ class Student
     print "id: #{id}\nsurname: #{surname}\nname: #{name}\nlastname: #{lastname}
 git: #{git}\nphone: #{phone}\ntg: #{tg}\nmail: #{mail}"
   end
+
   #валидаторы
   def self.valid_phone?(phone)
     phone.match(/^(\+7|8)((\(\d{3}\))|\d{3})\d{3}(-\d{2}-\d{2}|\d{4})$/)
@@ -39,6 +40,10 @@ git: #{git}\nphone: #{phone}\ntg: #{tg}\nmail: #{mail}"
     username.match(/^@[a-z]\w*$/)
   end
 
+  def self.valid_name?(name)
+    raise ArgumentError, "The argument must be String, not #{name.class}!" unless name.is_a?(String)
+    name.match(/^[a-zA-Z]+/)
+  end
   #есть ли гит и любой контакт для связи?(почта, телеграм или номер телефона)
   def validate?
     has_git? && has_contact?
@@ -49,7 +54,7 @@ git: #{git}\nphone: #{phone}\ntg: #{tg}\nmail: #{mail}"
   end
 
   def has_contact?
-    !(mail.nil? && tg.nil? && phone.nil?)
+    !mail.nil? || !tg.nil? || !phone.nil?
   end
 
   def set_contacts(phone: nil, tg: nil, mail: nil)
@@ -59,6 +64,20 @@ git: #{git}\nphone: #{phone}\ntg: #{tg}\nmail: #{mail}"
   end
 
   #сеттеры с проверкой
+
+  def surname=(surname)
+    raise ArgumentError, "Invalid surname value: #{surname}" if surname.nil? || !Student.valid_name?(surname)
+    @surname = surname
+  end
+
+  def name=(name)
+    raise ArgumentError, "Invalid name value: #{name}" if name.nil? || !Student.valid_name?(name)
+    @name = name
+  end
+  def lastname=(lastname)
+    raise ArgumentError, "Invalid lastname value: #{lastname}" if lastname.nil? || !Student.valid_name?(lastname)
+    @lastname = lastname
+  end
   def phone=(phone)
     raise ArgumentError, "Invalid phone value: #{phone}" if !phone.nil? && !Student.valid_phone?(phone)
     @phone = phone
@@ -79,5 +98,21 @@ git: #{git}\nphone: #{phone}\ntg: #{tg}\nmail: #{mail}"
     @git = git
   end
 
+  #возвращает строку с фамилией, инициалами и контактом для связи
+  def get_info
+    get_surname_and_initials + get_contacts
+  end
+  def get_contacts
+    contacts = ""
+    contacts += "git: #{git} "  if has_git?
+    contacts += "mail: #{mail} " unless mail.nil?
+    contacts += "tg: #{tg} " unless tg.nil?
+    contacts += "phone: #{phone} " unless phone.nil?
+    contacts
+  end
+  def get_surname_and_initials
+    "#{surname} #{name[0]}. #{lastname[0]}. "
+  end
 end
+
 
