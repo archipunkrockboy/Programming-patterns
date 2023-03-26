a0ee4ac18064f9393f6ca08a8379d7b9a932857d
archipunkrockboy <nowrim@mail.ru>
2023-03-26 16:46:07 +0300

    исключения для конструктора Student_short

--- Lab2/student_short.rb
+++ Lab2/student_short.rb
@@ -1,15 +1,21 @@
 require "json"
 class Student_short
+  attr_reader :surname_and_initials, :id, :git, :contact
   def initialize(id, str)
-    args = JSON.parse(str)
-    @surname_and_initials = args["surname_and_initials"]
-    @id = id
-    @git = args["git"]
-    @contact = args["contact"]
+    begin
+      args = JSON.parse(str)
+      raise ArgumentError, "#{str} doesn't contain information about student!" if args.nil?
+      @surname_and_initials = args["surname_and_initials"]
+      @id = id
+      @git = args["git"]
+      @contact = args["contact"]
+    rescue JSON::ParserError
+      p "Unable to extract data from #{str}"
+    end
   end
   def self.initialize_from_student(student)
     raise ArgumentError, "The argument must be Student, not #{student.class}!" unless student.is_a?(Student)
-    new(student.id, student.get_info)
+    Student_short.new(student.id, student.get_info)
   end
-
+  
 end
