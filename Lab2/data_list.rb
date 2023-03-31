0f8fe7291dbdc54bce4e1fd67e3ea4f925ab960e
archipunkrockboy <nowrim@mail.ru>
2023-03-31 13:44:55 +0300

    реализованы select, get_selected

--- Lab2/data_list.rb
+++ Lab2/data_list.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class Data_list
+  attr_accessor :selected_items
   def initialize(list)
     @list = list.sort
+    @selected_items = []
+  end
+
+  def select(number)
+    @selected_items.append(@list[number])
+  end
+
+  def get_selected
+    result = []
+    @list.map{|x| result << x if @selected_items.include?(x)}
+    result
   end
 end
