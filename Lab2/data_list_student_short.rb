e810110e74da13c670cbd4f356a458b95b6bef4d
archipunkrockboy <nowrim@mail.ru>
2023-04-07 00:53:54 +0300

    переопределил get_data и get_names

--- Lab2/data_list_student_short.rb
+++ Lab2/data_list_student_short.rb
@@ -1,4 +1,23 @@
-# frozen_string_literal: true
+class Data_list_student_short < Data_list
 
-class Data_list_student_short
+  def initialize(list)
+    list.each do |elem|
+      raise ArgumentError, "The elements of the list must be Student_short, not #{elem.class}!" unless elem.is_a?(Student_short)
+    end
+    super
+  end
+  def get_names
+    ["surname and initials", "git", "contact"]
+  end
+  def get_data
+    result = []
+    id = 0
+    list.each do |sh|
+      result << [id, sh.surname_and_initials, sh.git, sh.contact]
+      id += 1
+    end
+    Data_table.new(result)
+  end
+
+
 end
