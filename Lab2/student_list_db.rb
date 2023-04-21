c75894d2515ec6a3f5e74ff53f124857881a4adf
archipunkrockboy <nowrim@mail.ru>
2023-04-21 22:27:27 +0300

    получение студента по id

--- Lab2/student_list_db.rb
+++ Lab2/student_list_db.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-require './sqlite3'
+require 'sqlite3'
+require './student'
 class Student_list_db
   attr_accessor :db
 
@@ -8,4 +9,7 @@ class Student_list_db
     self.db.results_as_hash = results_as_hash
   end
 
+  def get_student_by_id(id)
+    Student.from_hash((db.execute "SELECT * FROM Students WHERE id == ?", id)[0])
+  end
 end
