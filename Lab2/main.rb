e1ec06ab1bec1065d60ce4a44d977de6798629ec
archipunkrockboy <nowrim@mail.ru>
2023-04-28 12:54:04 +0300

    доработка функционала+одиночка

--- Lab2/main.rb
+++ Lab2/main.rb
@@ -8,21 +8,27 @@ require './data_list_student_short'
 require './student_list_txt'
 require './student_list_json'
 require './student_list_yaml'
+require './student_list_db'
+require 'sqlite3'
 s1 = Student.new(name: "Sergey", surname: "Sergeev", lastname:"Sergeevich", id: 0, git: "@gitasdas")
-s2 = Student.new(name: "Petr", surname: "Sergeev", lastname:"Sergeevich", id: 1, git: "@gitadasd")
-s3 = Student.new(name: "Sergey", surname: "Petrov", lastname:"Sergeevich", id: 2, git: "@gitdasd")
-s4 = Student.new(name: "Sergey", surname: "Sergeev", lastname:"Petrovich", id: 3, git: "@gisadsat")
-s5 = Student.new(name: "Petr", surname: "Sergeev", lastname:"Petrovich", id: 4, git: "@giereet")
+# s2 = Student.new(name: "Petr", surname: "Sergeev", lastname:"Sergeevich", id: 1, git: "@gitadasd")
+s3 = Student.new(name: "Sergey", surname: "Petrov", lastname:"Sergeevich", git: "@gitdasd")
+# s4 = Student.new(name: "Sergey", surname: "Sergeev", lastname:"Petrovich", id: 3, git: "@gisadsat")
+# s5 = Student.new(name: "Petr", surname: "Sergeev", lastname:"Petrovich", id: 4, git: "@giereet")
+#
+# sl = Student_list.new(Student_list_json.new)
+# sl.read_from('input.json')
+# sl.write_to('output.json')
+#
+# sl = Student_list.new(Student_list_yaml.new)
+# sl.read_from('input.yaml')
+# sl.write_to('output.yaml')
+#
+# sl = Student_list.new(Student_list_txt.new)
+# sl.read_from('input.txt')
+# sl.write_to('output.txt')
 
-sl = Student_list.new(Student_list_json.new)
-sl.read_from('input.json')
-sl.write_to('output.json')
-
-sl = Student_list.new(Student_list_yaml.new)
-sl.read_from('input.yaml')
-sl.write_to('output.yaml')
-
-sl = Student_list.new(Student_list_txt.new)
-sl.read_from('input.txt')
-sl.write_to('output.txt')
+db = Student_list_db.new(1)
+p db.get_student_count
+p db.new_id
 
--- Lab2/student_list_db.rb
+++ Lab2/student_list_db.rb
@@ -2,11 +2,21 @@
 require 'sqlite3'
 require './student'
 class Student_list_db
-  attr_accessor :db
 
-  def initialize(db: "University.sqlite", results_as_hash: true)
-    self.db = SQLite3::Database.open db
-    self.db.results_as_hash = results_as_hash
+  private_class_method :new
+  @@instance = nil
+
+  def initialize(database)
+    self.set_parameters(database)
+  end
+
+  def self.get_instance(database: "University.sqlite")
+    if @@instance.nil? then
+      @@instance = new(database)
+    else
+      self.set_parameters(database)
+    end
+    @@instance
   end
 
   def get_student_by_id(id)
@@ -17,7 +27,37 @@ class Student_list_db
     db.execute "DELETE FROM Students WHERE id == ?", id
   end
 
-  def get_student_count
-    db.execute "SELECT COUNT(*) FROM Students"
+  def insert_student(student)
+    db.execute "INSERT INTO Students (id, surname, name, lastname, phone, tg, mail, git)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)", new_id, student.surname, student.name, student.lastname,
+                student.phone, student.tg, student.mail, student.git
   end
+  def update_student_by_id(id, student)
+    db.execute "UPDATE Students
+                SET surname = ?, name = ?, lastname = ?,
+                    phone   = ?, tg   = ?, mail     = ?,
+                    git = ?
+                WHERE id == ?", student.surname, student.name, student.lastname,
+                                student.phone, student.tg ,student.mail, student.git, id
+  end
+  def get_student_count
+    (db.execute "SELECT COUNT(*) as count FROM Students")[0]["count"]
+  end
+
+  def request(request)
+    db.execute request
+  end
+
+
+  private
+  attr_accessor :db, :instance
+  def set_parameters(db)
+    self.db = SQLite3::Database.open db
+    self.db.results_as_hash = true
+  end
+
+  def new_id
+    (db.execute "SELECT MAX(id) as max FROM Students")[0]["max"] + 1
+  end
+
 end
